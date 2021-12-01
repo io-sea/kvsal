3cf1069f47d7b646ca0f436dbf8b0325a242a115
Philippe DENIEL <philippe.deniel@cea.fr>
2021-12-01 08:30:27 -0500

    Fix rpm generation
    Change-Id: If0f9ab128be7b3a0c3279185841cc8ce11f1b6d3
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>
    Reviewed-on: https://gerrit.ccc.ocre.cea.fr/gerrit/c/kvsal/+/9932
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Yoann VALERI <yoann.valeri@cea.fr>

--- libkvsal.spec-in.cmake
+++ libkvsal.spec-in.cmake
@@ -1,11 +1,11 @@
 %define sourcename @CPACK_SOURCE_PACKAGE_FILE_NAME@
 %global dev_version %{lua: extraver = string.gsub('@LIBKVSAL_EXTRA_VERSION@', '%-', '.'); print(extraver) }
 
-Name: libkvsal 
+Name: libkvsal
 Version: @LIBKVSAL_BASE_VERSION@
 Release: 0%{dev_version}%{?dist}
 Summary: Library to abstract a KVS to be used by KVSNS
-License: LGPLv3 
+License: LGPLv3
 Group: Development/Libraries
 Url: http://github.com/phdeniel/libkvsal
 Source: %{sourcename}.tar.gz
@@ -78,7 +78,7 @@ This package contains tools for libkvsal.
 %setup -q -n %{sourcename}
 
 %build
-cmake . 
+cmake -DEXT_BUILD=@EXT_BUILD@ .
 
 make %{?_smp_mflags} || make %{?_smp_mflags} || make
 
@@ -125,4 +125,4 @@ rm -rf $RPM_BUILD_ROOT
 
 %changelog
 * Wed Nov  3 2021 Philippe DENIEL <philippe.deniel@cea.fr> 1.3.0
-- Better layering between kvsns, kvsal aand extstore. 
+- Better layering between kvsns, kvsal aand extstore.
